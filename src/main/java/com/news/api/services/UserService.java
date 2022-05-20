727b2ea3005bcf7262dcf30969aa70917ca4f427
Jonatas Caetano <jonatas.calves@gmail.com>
2022-05-20 17:23:28 -0300

    Added createAccount method

--- src/main/java/com/news/api/services/UserService.java
+++ src/main/java/com/news/api/services/UserService.java
@@ -3,6 +3,7 @@ package com.news.api.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.news.api.models.entities.User;
 import com.news.api.repositories.UserRepository;
 
 @Service
@@ -10,4 +11,18 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	public User createAccount(User user) {
+		return userRepository.insert(new User(
+				null,
+				user.getName(),
+				user.getEmail(),
+				user.getPassword(),
+				user.getCreationDate(),
+				user.getImage()
+				)
+			);
+	}
+	
+	
 }
