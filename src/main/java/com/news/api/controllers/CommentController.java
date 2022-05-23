9738457f804fcbb5853a7402c492d8324e250cd5
Jonatas Caetano <jonatas.calves@gmail.com>
2022-05-23 15:53:48 -0300

    Added getComment method of CommentService class

--- src/main/java/com/news/api/controllers/CommentController.java
+++ src/main/java/com/news/api/controllers/CommentController.java
@@ -1,9 +1,14 @@
 package com.news.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.news.api.models.entities.dtos.CommentDto;
 import com.news.api.services.CommentService;
 
 @RestController
@@ -12,4 +17,15 @@ public class CommentController {
 
 	@Autowired
 	private CommentService commentService;
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<CommentDto> getNews(@PathVariable String id){
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(commentService.getComment(id));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+	
 }
--- src/main/java/com/news/api/services/CommentService.java
+++ src/main/java/com/news/api/services/CommentService.java
@@ -3,6 +3,7 @@ package com.news.api.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.news.api.models.entities.dtos.CommentDto;
 import com.news.api.repositories.CommentRepository;
 
 @Service
@@ -10,4 +11,8 @@ public class CommentService {
 
 	@Autowired
 	private CommentRepository commentRepository;
+
+	public CommentDto getComment(String id){
+		return new CommentDto(commentRepository.findById(id).get());
+	}
 }
