335205ae9929fbed5cc38434aa18b54c0b7e4e20
Jonatas Caetano <jonatas.calves@gmail.com>
2022-05-23 15:44:36 -0300

    Added the getNews method of the NewsService class

--- src/main/java/com/news/api/controllers/NewsController.java
+++ src/main/java/com/news/api/controllers/NewsController.java
@@ -1,9 +1,14 @@
 package com.news.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.news.api.models.entities.dtos.NewsDto;
 import com.news.api.services.NewsService;
 
 @RestController
@@ -12,4 +17,14 @@ public class NewsController {
 
 	@Autowired
 	private NewsService newsService;
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<NewsDto> getNews(@PathVariable String id){
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(newsService.getNews(id));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
 }
--- src/main/java/com/news/api/services/NewsService.java
+++ src/main/java/com/news/api/services/NewsService.java
@@ -3,6 +3,7 @@ package com.news.api.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.news.api.models.entities.dtos.NewsDto;
 import com.news.api.repositories.NewsRepository;
 
 @Service
@@ -10,4 +11,8 @@ public class NewsService {
 
 	@Autowired
 	private NewsRepository newsRepository;
+
+	public NewsDto getNews(String id){
+		return new NewsDto(newsRepository.findById(id).get());
+	}
 }
